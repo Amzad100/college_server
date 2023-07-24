b1a90ca6f60eb417469f81ac59ca19938a58a2d9
Amzad100 <mdamzad2003a@gmail.com>
2023-07-24 23:29:37 +0600

    final commit

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const express = require('express');
 const app = express();
+require('dotenv').config()
 const cors = require('cors');
 const port = process.env.PORT || 5000;
-// const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-// require('dotenv').config()
 
 app.use(cors());
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://<username>:<password>@cluster0.73bu6ml.mongodb.net/?retryWrites=true&w=majority";
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.73bu6ml.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -25,12 +24,58 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const collegesCollection = client.db('collegeDB').collection('college');
+        const usersCollection = client.db('collegeDB').collection('users');
+        const reviewsCollection = client.db('collegeDB').collection('reviews');
+
+        // user api
+        app.get('/users', async (req, res) => {
+            const result = await usersCollection.find().toArray()
+            res.send(result)
+        })
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email }
+            const existingUser = await usersCollection.findOne(query)
+            if (existingUser) {
+                return res.send({ message: "user already exist" })
+            }
+            const result = await usersCollection.insertOne(user)
+            res.send(result)
+        });
+        
+        // Reviews api
+        app.get('/reviews', async (req, res) => {
+            const result = await reviewsCollection.find().toArray()
+            res.send(result)
+        })
+
+        // colleges api
+        app.get('/colleges', async (req, res) => {
+            const result = await collegesCollection.find({}).toArray();
+            res.send(result);
+        })
+
+        app.get('/colleges/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+
+            const options = {
+                projection: { college_name: 1, admission_dates: 1, research_history: 1, college_image: 1, _id: 1, events: 1, sports: 1 },
+            };
+
+            const result = await collegesCollection.findOne(query, options)
+            res.send(result);
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
